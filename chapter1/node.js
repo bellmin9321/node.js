64a872df6c85cbec4e2cdd18917489c0a9e959e5
bellmin9321 <bellmin9321@gmail.com>
2021-01-22 00:36:26 +0900

    use promise, then

--- chapter1/node.js
+++ chapter1/node.js
@@ -1,12 +1,35 @@
-function first() {
-  second();
-  console.log('첫 번째');
+// function first() {
+//   second();
+//   console.log('첫 번째');
+// }
+// function second() {
+//   third();
+//   console.log('두 번째');
+// }
+// function third() {
+//   console.log('세 번째');
+// }
+// first();
+
+// function run() {
+//   console.log('3초 후 실행');
+// }
+// console.log('시작');
+// setTimeout(run, 3000);
+// console.log('끝');
+function oneMore() {
+  console.log('one more');
 }
-function second() {
-  third();
-  console.log('두 번째');
+function run() {
+  console.log('run run')
+  setTimeout(() => {
+    console.log('wow');
+  }, 0)
+  new Promise((resolve) => {
+    resolve('hi');
+  })
+    .then(console.log);
+  oneMore();
 }
-function third() {
-  console.log('세 번째');
-}
-first();
+
+setTimeout(run, 5000);
