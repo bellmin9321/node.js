ad5a1c7f6d24fdf75303c73b9f4208ae9ea10f4a
bellmin9321 <bellmin9321@gmail.com>
2021-02-21 15:57:34 +0900

    user, 댓글 by sequelize

--- 6chapter/models/user.js
+++ 6chapter/models/user.js
@@ -1,23 +1,25 @@
 const { Sequelize } = require('sequelize');
 
 module.exports = class User extends Sequelize.Model {
+  // sequelize는 id를 자동으로 생성하므로 생략
   static init(sequelize) {
     return super.init({
-      name: {
-        Sequelize.toString(20),
-        allowNull: false,
-        unique: true,
+    name: {
+      type: Sequelize.toString(20),
+      allowNull: false,
+      unique: true,
     },
     age: {
       type: Sequelize.TINYINT.UNSIGNED,
       allowNull: false,
     },
     married: {
-      type: Sequelize.BOOLEAN,
+      type: Sequelize.BOOLEAN, // ture, false
       allowNull: false,
     },
     comment: {
-      type: Sequelize.TETX,
+      type: Sequelize.TEXT,
+      allowNull: true,
     },
     created_at: {
       type: Sequelize.DATE, // DATETIME, MYSQL -> Sequelize DateOnly
@@ -35,4 +37,5 @@ module.exports = class User extends Sequelize.Model {
     charset: 'utf8mb4',
     collate: 'utf8mb4_general_ci',
   });
+ }
 }
