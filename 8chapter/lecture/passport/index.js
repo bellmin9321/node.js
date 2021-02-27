40b4c9115e1a390cfda8058574e787b36fc86ddc
bellmin9321 <bellmin9321@gmail.com>
2021-02-27 23:08:06 +0900

    kakao login

--- 8chapter/lecture/passport/index.js
+++ 8chapter/lecture/passport/index.js
@@ -5,13 +5,14 @@ const User = require('../models/user');
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user.id); // 세션에 user의 id만 저장
   });
+
+  // { id: 3, 'connect.sid': s%3143141513513 }
 
   passport.deserializeUser((id, done) => {
     User.findOne({ where: { id } })
-      .then(user => done(null, user))
-      .catch(err => done(err));
+      .then(user => done(null, user)) //req.user, req.isAuthenticated()
   });
 
   local();
